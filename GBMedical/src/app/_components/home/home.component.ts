b166c590d1b2fbd60d3946f0f8ca5fb7205bcb06
SzGeri25 <szabo.gergely2@simonyiszki.org>
2024-12-13 12:13:09 +0100

    Navbar létehozása, logo beszúrása

--- GBMedical/src/app/_components/home/home.component.ts
+++ GBMedical/src/app/_components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
