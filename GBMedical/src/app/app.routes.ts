dc49dd100c9f2454cff4455278f25c00141eb420
SzGeri25 <szabo.gergely2@simonyiszki.org>
2024-12-16 15:02:49 +0100

    Navbar + főoldal kódolgatás

--- GBMedical/src/app/app.routes.ts
+++ GBMedical/src/app/app.routes.ts
@@ -4,5 +4,9 @@ import { HomeComponent } from './_components/home/home.component';
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/home' },
     { path: 'home', component: HomeComponent },
+    
 
+
+
+    { path: '**', redirectTo: '' } // 404 esetén visszairányítás a Home-ra, ez legyen legalul!!!
 ];
