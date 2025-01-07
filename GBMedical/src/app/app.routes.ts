d11ee045bab1cb4e81f9c833d32f94fa5f4f988f
bogdanbalazs <158621597+bogdanbalazs@users.noreply.github.com>
2025-01-07 12:45:59 +0100

    merge test

---
+++ GBMedical/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './_components/home/home.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'home', component: HomeComponent },
+    
+
+
+
+    { path: '**', redirectTo: '' } // 404 esetén visszairányítás a Home-ra, ez legyen legalul!!!
+];
