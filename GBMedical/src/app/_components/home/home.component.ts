4a84744b98b3bd2806fcaed1b70bead1be409dff
bogdanbalazs <158621597+bogdanbalazs@users.noreply.github.com>
2025-01-14 13:56:03 +0100

    Navbar elkezdése

--- GBMedical/src/app/_components/home/home.component.ts
+++ GBMedical/src/app/_components/home/home.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
-import { TotopComponent } from '../totop/totop.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FormsModule, FooterComponent, TotopComponent],
+  imports: [NavbarComponent, FormsModule, FooterComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
