3072a5c1cf3d6390a8c80188dca39fd4bed1e736
SzGeri25 <szabo.gergely2@simonyiszki.org>
2025-02-10 15:42:34 +0100

    Sikeres szolgáltatások betöltés

--- GBMedical/src/app/_components/services/services.component.ts
+++ GBMedical/src/app/_components/services/services.component.ts
@@ -1,21 +1,49 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+interface Service {
+  id: number;
+  name: string;
+  description: string;
+  doctor_names: string[];
+  price: number;
+  duration: number;
+}
 
 @Component({
   selector: 'app-services',
-  imports: [FormsModule, CommonModule],
+  imports: [NavbarComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-  services$!: Observable<any[]>;
+  services: Service[] = [];
+  errorMessage: string = '';
+  private apiUrl = 'http://127.0.0.1:8080/GBMedicalBackend-1.0-SNAPSHOT/webresources/services/getAllServices';
 
-  constructor(private http: HttpClient) {}
+  constructor() { }
 
-  ngOnInit() {
-    this.services$ = this.http.get<any[]>('http://127.0.0.1:8080/GBMedicalBackend-1.0-SNAPSHOT/webresources/services');
+  ngOnInit(): void {
+    this.fetchServices();
+  }
+
+  fetchServices(): void {
+    fetch(this.apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Hálózati hiba: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data: { services: Service[] }) => {
+        this.services = data.services;
+      })
+      .catch((error) => {
+        console.error('Hiba történt az adatok lekérésekor:', error);
+        this.errorMessage = 'Nem sikerült betölteni az adatokat.';
+      });
   }
 }
