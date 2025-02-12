c337b8fab4193ffb104295aee4711c92298d9ac9
SzGeri25 <szabo.gergely2@simonyiszki.org>
2025-02-12 11:05:07 +0100

    Csak töröltem felesleges importokat

--- GBMedical/src/app/_components/home/home.component.ts
+++ GBMedical/src/app/_components/home/home.component.ts
@@ -2,15 +2,10 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
-import { LoginComponent } from "../login/login.component";
-import { RegisterComponent } from "../register/register.component";
-import { CalendarComponent } from '../calendar/calendar.component';
-import { ContactComponent } from "../contact/contact.component";
-import { ServicesComponent } from '../services/services.component';
 
 @Component({
     selector: 'app-home',
-    imports: [NavbarComponent, FormsModule, FooterComponent, LoginComponent, RegisterComponent, CalendarComponent, ContactComponent, ServicesComponent],
+    imports: [NavbarComponent, FormsModule, FooterComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
