93137d3bfceeb26fc2418ab0b4049d8b53ac09f2
SzGeri25 <szabo.gergely2@simonyiszki.org>
2025-02-27 10:46:56 +0100

    Főoldal szépítgetése

--- GBMedical/src/app/_components/home/home.component.ts
+++ GBMedical/src/app/_components/home/home.component.ts
@@ -2,13 +2,30 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
+import { trigger, style, animate, transition } from '@angular/animations';
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-home',
     imports: [NavbarComponent, FormsModule, FooterComponent],
     templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+    styleUrl: './home.component.css',
+    animations: [
+        // Egyszerű beúszó animáció a komponenselemekhez
+        trigger('fadeIn', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateY(20px)' }),
+                animate('800ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+            ])
+        ])
+    ]
 })
 export class HomeComponent {
 
+constructor(private router: Router){}
+
+    goToBooking(): void {
+        this.router.navigate(['/calendar']);
+    }
 }
